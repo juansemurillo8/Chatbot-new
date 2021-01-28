86929271007b720582f168db6268ba675d3283e5
juansemurillo8 <78004175+juansemurillo8@users.noreply.github.com>
2021-01-27 23:17:05 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,245 @@ import json
 import requests
 from flask import Flask, request
 
+# Adicional
+"""Practica1_chatbot.ipynb
+Limpa el Corpus
+"""
+import numpy as np
+#import pandas as pd
+#from matplotlib import pyplot as plt
+#import matplotlib.pyplot as plt
+#from pathlib import Path
+#from strings import string
+import unidecode
+#import nltk
+#from nltk.corpus import stopwords
+
+#np.random.seed(seed=0)
+
+#import tensorflow as tf
+#tf.random.set_seed(0)
+"""
+from google.colab import drive
+drive.mount('/content/drive', force_remount=True)
+root_dir = "/content/drive/My Drive/"
+base_dir = root_dir + 'practica1_topicos1/'
+
+model_dict = base_dir + 'Modelos descargados/'
+
+
+path=Path(base_dir)
+print(path)
+"""
+
+#nltk.download('stopwords')
+
+stop_words = ['de', 'la', 'que', 'el', 'en', 'y', 'a', 'los', 'del', 'se', 'las', 'por', 'un', 'para', 'con', 'no', 'una', 'su', 'al', 'lo', 'como', 'más', 'pero', 'sus', 'le', 'ya', 'o', 'este', 'sí', 'porque', 'esta', 'entre', 'cuando', 'muy', 'sin', 'sobre', 'también', 'me', 'hasta', 'hay', 'donde', 'quien', 'desde', 'todo', 'nos', 'durante', 'todos', 'uno', 'les', 'ni', 'contra', 'otros', 'ese', 'eso', 'ante', 'ellos', 'e', 'esto', 'mí', 'antes', 'algunos', 'qué', 'unos', 'yo', 'otro', 'otras', 'otra', 'él', 'tanto', 'esa', 'estos', 'mucho', 'quienes', 'nada', 'muchos', 'cual', 'poco', 'ella', 'estar', 'estas', 'algunas', 'algo', 'nosotros', 'mi', 'mis', 'tú', 'te', 'ti', 'tu', 'tus', 'ellas', 'nosotras', 'vosotros', 'vosotras', 'os', 'mío', 'mía', 'míos', 'mías', 'tuyo', 'tuya', 'tuyos', 'tuyas', 'suyo', 'suya', 'suyos', 'suyas', 'nuestro', 'nuestra', 'nuestros', 'nuestras', 'vuestro', 'vuestra', 'vuestros', 'vuestras', 'esos', 'esas', 'estoy', 'estás', 'está', 'estamos', 'estáis', 'están', 'esté', 'estés', 'estemos', 'estéis', 'estén', 'estaré', 'estarás', 'estará', 'estaremos', 'estaréis', 'estarán', 'estaría', 'estarías', 'estaríamos', 'estaríais', 'estarían', 'estaba', 'estabas', 'estábamos', 'estabais', 'estaban', 'estuve', 'estuviste', 'estuvo', 'estuvimos', 'estuvisteis', 'estuvieron', 'estuviera', 'estuvieras', 'estuviéramos', 'estuvierais', 'estuvieran', 'estuviese', 'estuvieses', 'estuviésemos', 'estuvieseis', 'estuviesen', 'estando', 'estado', 'estada', 'estados', 'estadas', 'estad', 'he', 'has', 'ha', 'hemos', 'habéis', 'han', 'haya', 'hayas', 'hayamos', 'hayáis', 'hayan', 'habré', 'habrás', 'habrá', 'habremos', 'habréis', 'habrán', 'habría', 'habrías', 'habríamos', 'habríais', 'habrían', 'había', 'habías', 'habíamos', 'habíais', 'habían', 'hube', 'hubiste', 'hubo', 'hubimos', 'hubisteis', 'hubieron', 'hubiera', 'hubieras', 'hubiéramos', 'hubierais', 'hubieran', 'hubiese', 'hubieses', 'hubiésemos', 'hubieseis', 'hubiesen', 'habiendo', 'habido', 'habida', 'habidos', 'habidas', 'soy', 'eres', 'es', 'somos', 'sois', 'son', 'sea', 'seas', 'seamos', 'seáis', 'sean', 'seré', 'serás', 'será', 'seremos', 'seréis', 'serán', 'sería', 'serías', 'seríamos', 'seríais', 'serían', 'era', 'eras', 'éramos', 'erais', 'eran', 'fui', 'fuiste', 'fue', 'fuimos', 'fuisteis', 'fueron', 'fuera', 'fueras', 'fuéramos', 'fuerais', 'fueran', 'fuese', 'fueses', 'fuésemos', 'fueseis', 'fuesen', 'sintiendo', 'sentido', 'sentida', 'sentidos', 'sentidas', 'siente', 'sentid', 'tengo', 'tienes', 'tiene', 'tenemos', 'tenéis', 'tienen', 'tenga', 'tengas', 'tengamos', 'tengáis', 'tengan', 'tendré', 'tendrás', 'tendrá', 'tendremos', 'tendréis', 'tendrán', 'tendría', 'tendrías', 'tendríamos', 'tendríais', 'tendrían', 'tenía', 'tenías', 'teníamos', 'teníais', 'tenían', 'tuve', 'tuviste', 'tuvo', 'tuvimos', 'tuvisteis', 'tuvieron', 'tuviera', 'tuvieras', 'tuviéramos', 'tuvierais', 'tuvieran', 'tuviese', 'tuvieses', 'tuviésemos', 'tuvieseis', 'tuviesen', 'teniendo', 'tenido', 'tenida', 'tenidos', 'tenidas', 'tened']
+print('debug0')
+def limpiezatexto(text):
+  text = text.replace('-',' ')
+  text = text.replace('/',' ')
+  #text = text.translate(str.maketrans('', '', string.punctuation))
+  text = text.lower()
+  text = ' '.join([word for word in text.split() if word not in stop_words])
+  text = unidecode.unidecode(text)
+  return text
+
+import re
+#import random
+#with open(path/'preguntas_Literatura.txt',mode='r',encoding='utf-8') as f:
+with open('preguntas_Literatura.txt',mode='r',encoding='utf-8') as f:
+  lines=f.read().split('\n')
+#with open(path/'respuestas_Literatura.txt',mode='r',encoding='utf-8') as f:
+with open('respuestas_Literatura.txt',mode='r',encoding='utf-8') as f:
+  lines2=f.read().split('\n')
+  lines=[re.sub(r"\[\w+\]",'',line) for line in lines]
+  lines=[" ".join(re.findall(r"\w+",line)) for line in lines]
+  lines2=[re.sub(r"\[\w+\]",'',line) for line in lines2]
+  lines2=[" ".join(re.findall(r"\w+",line)) for line in lines2]
+  lines = list(map(limpiezatexto, lines))
+  lines2 = list(map(limpiezatexto, lines2))
+  pairs=list(zip(lines,lines2))
+  #random.shuffle(pairs)
+
+import numpy as np
+input_docs = []
+target_docs = []
+input_tokens = set()
+target_tokens = set()
+for line in pairs[:]:
+  input_doc, target_doc = line[0], line[1]
+  # Appending each input sentence to input_docs
+  input_docs.append(input_doc)
+  # Splitting words from punctuation  
+  target_doc = " ".join(re.findall(r"[\w']+|[^\s\w]", target_doc))
+  # Redefine target_doc below and append it to target_docs
+  target_doc = '<START> ' + target_doc + ' <END>'
+  target_docs.append(target_doc)
+  
+  # Now we split up each sentence into words and add each unique word to our vocabulary set
+  for token in re.findall(r"[\w']+|[^\s\w]", input_doc):
+    if token not in input_tokens:
+      input_tokens.add(token)
+  for token in target_doc.split():
+    if token not in target_tokens:
+      target_tokens.add(token)
+input_tokens = sorted(list(input_tokens))
+target_tokens = sorted(list(target_tokens))
+num_encoder_tokens = len(input_tokens)
+num_decoder_tokens = len(target_tokens)
+
+input_features_dict = dict(
+    [(token, i) for i, token in enumerate(input_tokens)])
+target_features_dict = dict(
+    [(token, i) for i, token in enumerate(target_tokens)])
+reverse_input_features_dict = dict(
+    (i, token) for token, i in input_features_dict.items())
+reverse_target_features_dict = dict(
+    (i, token) for token, i in target_features_dict.items())
+
+#Maximum length of sentences in input and target documents
+max_encoder_seq_length = max([len(re.findall(r"[\w']+|[^\s\w]", input_doc)) for input_doc in input_docs])
+max_decoder_seq_length = max([len(re.findall(r"[\w']+|[^\s\w]", target_doc)) for target_doc in target_docs])
+encoder_input_data = np.zeros(
+    (len(input_docs), max_encoder_seq_length, num_encoder_tokens),
+    dtype='float32')
+decoder_input_data = np.zeros(
+    (len(input_docs), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+decoder_target_data = np.zeros(
+    (len(input_docs), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+for line, (input_doc, target_doc) in enumerate(zip(input_docs, target_docs)):
+    for timestep, token in enumerate(re.findall(r"[\w']+|[^\s\w]", input_doc)):
+        #Assign 1. for the current line, timestep, & word in encoder_input_data
+        encoder_input_data[line, timestep, input_features_dict[token]] = 1.
+    
+    for timestep, token in enumerate(target_doc.split()):
+        decoder_input_data[line, timestep, target_features_dict[token]] = 1.
+        if timestep > 0:
+            decoder_target_data[line, timestep - 1, target_features_dict[token]] = 1.
+
+print('debug1',max_encoder_seq_length)
+
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Dense, LSTM
+#from keras.layers import Input, LSTM, Dense
+from tensorflow.keras.models import Model
+#Dimensionality
+dimensionality = 256
+#The batch size and number of epochs
+batch_size = 5
+epochs = 300
+#Encoder
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder_lstm = LSTM(dimensionality, return_state=True)
+encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs)
+encoder_states = [state_hidden, state_cell]
+#Decoder
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+decoder_lstm = LSTM(dimensionality, return_sequences=True, return_state=True)
+decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+#Model
+training_model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+from tensorflow.keras.models import load_model
+training_model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+#training_model.load_weights(model_dict + 'training_model Test.h5')# Cambiar path 
+training_model.load_weights('Models/training_model Test.h5')
+encoder_inputs = training_model.input[0]
+encoder_outputs, state_h_enc, state_c_enc = training_model.layers[2].output
+encoder_states = [state_h_enc, state_c_enc]
+encoder_model = Model(encoder_inputs, encoder_states)
+
+latent_dim = 256
+decoder_state_input_hidden = Input(shape=(latent_dim,))
+decoder_state_input_cell = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_hidden, decoder_state_input_cell]
+decoder_outputs, state_hidden, state_cell = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_hidden, state_cell]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+
+def decode_response(test_input):
+  #Getting the output states to pass into the decoder
+  states_value = encoder_model.predict(test_input)
+  #Generating empty target sequence of length 1
+  target_seq = np.zeros((1, 1, num_decoder_tokens))
+  #Setting the first token of target sequence with the start token
+  target_seq[0, 0, target_features_dict['<START>']] = 1.
+  
+  #A variable to store our response word by word
+  decoded_sentence = ''
+  
+  stop_condition = False
+  while not stop_condition:
+      #Predicting output tokens with probabilities and states
+    output_tokens, hidden_state, cell_state = decoder_model.predict([target_seq] + states_value)
+  #Choosing the one with highest probability
+    sampled_token_index = np.argmax(output_tokens[0, -1, :])
+    sampled_token = reverse_target_features_dict[sampled_token_index]
+    decoded_sentence += " " + sampled_token
+  #Stop if hit max length or found the stop token
+    if (sampled_token == '<END>' or len(decoded_sentence) > max_decoder_seq_length):
+      stop_condition = True
+  #Update the target sequence
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    target_seq[0, 0, sampled_token_index] = 1.
+    #Update states
+    states_value = [hidden_state, cell_state]
+  return decoded_sentence
+
+class ChatBot:
+  negative_responses = ("no", "nope", "nah", "naw", "not a chance", "sorry")
+  exit_commands = ("quit", "pause", "exit", "goodbye", "bye", "later", "stop", "salir", "pausar", "chao", "detener", "hasta luego")
+#Method to start the conversation
+  def start_chat(self):
+    user_response = input("Hola, soy un chatbot listo para responder tus preguntas de literura, ¿En qué te puedo ayudar?\n")
+    
+    if user_response in self.negative_responses:
+      print("Que tengas un bonito día!")
+      return
+    self.chat(user_response)
+#Method to handle the conversation
+  def chat(self, reply):
+    while not self.make_exit(reply):
+      reply = input(self.generate_response(reply)+"\n")
+    
+  #Method to convert user input into a matrix
+  def string_to_matrix(self, user_input):
+    tokens = re.findall(r"[\w']+|[^\s\w]", user_input)
+    user_input_matrix = np.zeros(
+      (1, max_encoder_seq_length, num_encoder_tokens),
+      dtype='float32')
+    for timestep, token in enumerate(tokens):
+      if token in input_features_dict:
+        user_input_matrix[0, timestep, input_features_dict[token]] = 1.
+    return user_input_matrix
+  
+  #Method that will create a response using seq2seq model we built
+  def generate_response(self, user_input):
+    input_matrix = self.string_to_matrix(user_input)
+    chatbot_response = decode_response(input_matrix)
+    #Remove <START> and <END> tokens from chatbot_response
+    chatbot_response = chatbot_response.replace("<START>",'')
+    chatbot_response = chatbot_response.replace("<END>",'')
+    return chatbot_response
+#Method to check for exit commands
+  def make_exit(self, reply):
+    for exit_command in self.exit_commands:
+      if exit_command in reply:
+        print("Ok, Que tengas un bonito día!")
+        return True
+    return False
+  
+chatbot = ChatBot()
+print('Debug2',chatbot.generate_response('como estas'))
+#Hasta aquí
+
 app = Flask(__name__)
 
 
@@ -48,8 +287,11 @@ def webhook():
                     recipient_id = messaging_event['recipient']['id']  # el facebook ID de la pagina que recibe (tu pagina)
                     message_text = messaging_event['message']['text']  # el texto del mensaje
 
-                    # Aqui es donde se debe reemplazar la respuesta fija por la generada por la red neuronal ya entrenada
-                    send_message(sender_id, 'Hola, soy siempre la misma respuesta')
+## Parte modificada
+                    respuesta = ChatBot().generate_response(message_text)
+
+                    send_message(sender_id, respuesta)
+## Fin parte modificada
 
                 if messaging_event.get('delivery'):  # confirmacion de delivery
                     pass
